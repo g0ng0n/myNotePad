d12edde3933f7f36753cdecb8948dda2fe40a41f
Gonzalo Gisbert <gonzalo.a.gisbert@gmail.com>
2017-09-17 19:27:10 -0300

    adding dependencies iin order to use redux and refactor the code

--- src/App.js
+++ src/App.js
@@ -26,82 +26,12 @@ class App extends Component {
     } 
   }
 
-  componentWillMount() {
-    firebase.initializeApp({
-        apiKey: "AIzaSyDXf8A1LXSfOM8b0Hb02gX1uBxrLzIwfRM",
-        authDomain: "notepad-ce4af.firebaseapp.com",
-        databaseURL: "https://notepad-ce4af.firebaseio.com",
-        projectId: "notepad-ce4af",
-        storageBucket: "",
-        messagingSenderId: "180317633862"
-      }
-    );
-
-    console.log('Firebase success');
-
-    firebase.database().ref('/notes')
-      .on('value', snapshot => {
-        const fbstore = snapshot.val();
-
-        const store = _.map(fbstore, (value, id) =>{
-
-          return {
-            id: id,
-            title: value.title,
-            details: value.details,
-          };
-        });
-        this.setState({
-          notes: store
-        })
-      });
-  
-  }
-
-  handleChange(event){
-    const name = event.target.name;
-    const value = event.target.value;
-
-    this.setState({
-      [name] : value
-    });
-  }
-
-  handleSubmit(event){
-   
-   event.preventDefault();
-
-   const data = {
-    title: this.state.currentTitle,
-    details: this.state.currentDetails,
-   };
-
-   firebase.database().ref('/notes').push(data, response => response);
-
-   this.setState({
- 
-    currentTitle: '',
-    currentDetails: ''
-   });
-
-
-  }
-
-  deleteNote(id){
-    firebase.database().ref(`/notes/${id}`).remove();
-    console.log(`note with ${id} has been successfully deleted`);
-  }
  render() {
     return (
       <div style={styles}>   
         <Header name={this.state.name}/>
-        <Form 
-          currentTitle={this.state.currentTitle}
-          currentDetails={this.state.currentDetails} 
-          handleChange={this.handleChange.bind(this)}
-          handleSubmit={this.handleSubmit.bind(this)}
-        />
-        <Grid notes={this.state.notes} deleteNote={this.deleteNote.bind(this)}/>
+        <Form />
+        <Grid />
 
       </div>
     );
